f6187c1125d130769145a455c473d20810e23cd2
Fredrick Otieno Nyambok <frednyambok2030@gmail.com>
2023-03-01 11:07:12 +0300

    new

--- vineyard2/mysite/mysite/settings.py
+++ vineyard2/mysite/mysite/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(BASE_DIR.joinpath('templates')),],
+        'DIRS': [str(p) for p in BASE_DIR.joinpath('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
