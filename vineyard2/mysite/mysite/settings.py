8bced0c5f638dec20573c0495c80ebc185b87f40
Fredrick Otieno Nyambok <frednyambok2030@gmail.com>
2023-03-01 09:05:35 +0300

    more

--- vineyard2/mysite/mysite/settings.py
+++ vineyard2/mysite/mysite/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = '/static/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/'
 
